407a06ab6eb08ce2006867ac76a83e8574e4bf80
Eldrick Nartea <eldrick.nartea@accenture.com>
2025-05-23 05:03:51 +0000

    Activity Commit

--- webapp/model/models.js
+++ webapp/model/models.js
@@ -14,7 +14,21 @@ function (JSONModel, Device) {
             var oModel = new JSONModel(Device);
             oModel.setDefaultBindingMode("OneWay");
             return oModel;
-        }
+        },
+        
+formatMail: function(sEid) {
+      var oBundle = this.getView().getModel("i18n").getResourceBundle();
+      return mobileLibrary.URLHelper.normalizeEmail(
+        sEid + oBundle.getText("domain"),
+        oBundle.getText("mailSubject", [sEid]),
+        oBundle.getText("mailBody")
+      );
+    },
+    
+    formatStockValue: function(fUnitPrice, iStockLevel, sCurrCode) {
+      var oCurrency = new sap.ui.model.type.Currency();
+      return oCurrency.formatValue([fUnitPrice * iStockLevel, sCurrCode], "string");
+    }
     };
 
 });
